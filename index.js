12b413d601d5ebb72264511f68514919e487ec1f
ScuffedDev <45866296+ScuffedDev@users.noreply.github.com>
2019-02-26 23:19:04 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const WebSocket = require('ws');
 var express = require('express');
+const chalk = require('chalk');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server); 
@@ -13,7 +14,8 @@ var json = {
 }
 
 const wss = new WebSocket.Server({ port: 8974 });
- 
+console.log(chalk.bgGreen.white('websockets')+chalk.blue(' started on port 8974'));
+
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
     //console.log('received: %s', message);
@@ -35,14 +37,16 @@ wss.on('connection', function connection(ws) {
   }); 
 });
 
-server.listen(8080);
 
+server.listen(8080);
+console.log(chalk.bgGreen.white('socket.io') + chalk.blue(' started on port 8080'));
 app.use(express.static('public'))
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
-  
+console.log(chalk.bgGreen.white('webserver') + chalk.blue(' started on port 8080'));
+
 
 
 io.on('connection', function (socket) {
@@ -58,3 +62,6 @@ setInterval(function(){
     io.emit('message', JSON.stringify(json));
     //console.log(json)
 },1000)
+
+console.log(chalk.green('\n\n\nOverlay Address: http://localhost:8080'));
+console.log(chalk.gray("To close, press 'ctrl+c'."));
